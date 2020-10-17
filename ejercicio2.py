9ab4b572da1762526f2b04f930216a289e34d03b
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 17:28:01 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+'''
+-----------------------------
+ EJERCICIO N°2
+ Bucle for
+-----------------------------
+NOTA: Realiza cambios en range(), prueba con range(1,1)
+¿Qué sucede?
+-----------------------------
+'''
+for i in range(2, 8, 3):
+  print("El valor de i es actualmente", i)
