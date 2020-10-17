293bdba6bb54b59bae8e4b537c49d4ba9266f489
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 15:58:30 -0500

    Create reto1.py

---
+++ reto1.py
@@ -0,0 +1,27 @@
+'''
+-----------------------------
+ RETO N°1
+ Bucle while
+-----------------------------
+Tu tarea es ayudar al mago a completar el código del juego "Adivina el número secreto", el cual debe:
+ 1. Pedir al usuario que ingrese un número entero.
+ 2. Utilizar un ciclo while.
+ 3. Comprobar si el número ingresado por el usuario es el mismo que el escogido por el mago. Si es diferente, 
+ el usuario verá el mensaje "¡Ja, ja! ¡Estás atrapado en mi ciclo!" y se pedirá que ingrese un nuevo número. 
+ 4. Si los números coinciden, el número se imprime en la pantalla, y el mago debe decir: "¡Bien hecho, 
+ muggle! Eres libre ahora".
+-----------------------------
+'''
+numeroSecreto = 777
+
+print(
+"""
++==================================+
+| Bienvenido a mi juego, muggle!   |
+| Introduce un número entero       |
+| y adivina qué número he          |
+| elegido para ti.                 |
+| Entonces,                        |
+| ¿Cuál es el número secreto?      |
++==================================+
+""")
