33c03e65dff39c5f6b09149aee5af8224a19d193
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 16:43:15 -0500

    Create reto2.py

---
+++ reto2.py
@@ -0,0 +1,18 @@
+'''
+-----------------------------
+ RETO N°2
+ Bucle for
+-----------------------------
+¿Sabías que agregar la palabra Mississippi a un número al contar los segundos en voz alta hace que suene más cercano al reloj? A menudo lo usan los niños que juegan al escondite para asegurarse de que el buscador haga un conteo honesto.
+
+Completa el programa para que use un ciclo for para "contar de forma mississippi" hasta cinco.
+Luego de contar, el programa debe imprimir "¡Listo o no, ahí voy!"
+-----------------------------
+'''
+import time
+
+# Escribe un bucle for que cuente hasta cinco.
+  # Cuerpo del bucle: imprime el número de iteración del ciclo y la palabra "Mississippi".
+  # Cuerpo del bucle - usar: time.sleep (1)
+
+# Escribe una función de impresión con el mensaje final.
