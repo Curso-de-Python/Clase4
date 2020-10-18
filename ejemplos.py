7f2965060043a911dfd5f81281c4cd81cfd2a572
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 20:04:46 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,45 @@
+# DULCES SINTÁCTICOS
+
+# --------------------
+# Break
+
+print("La instrucción de ruptura:")
+
+for i in range(1, 6):
+  if i == 3:
+    break
+  print("Dentro del ciclo.", i)
+print("Fuera del ciclo")
+
+'''
+# --------------------
+# Continue
+
+print("La instrucción continue:")
+
+for i in range(1, 6):
+  if i == 3:
+    continue
+  print("Dentro del ciclo.", i)
+print("Fuera del ciclo")
+
+# --------------------
+# WHILE Y LA RAMA ELSE
+# Probar con i = 5
+
+i = 1
+while i < 5:
+  print(i)
+  i += 1
+else:
+  print("else: ", i)
+
+# --------------------
+# FOR Y LA RAMA ELSE
+# Probar con range(2,1)
+
+for i in range(5):
+  print(i)
+else:
+  print("else: ", i)
+'''
