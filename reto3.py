a94abb3d656cbc9b2b0ad4f024eef83601762343
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 20:05:45 -0500

    Update reto3.py

--- reto3.py
+++ reto3.py
@@ -4,10 +4,9 @@
  Bucle for
 -----------------------------
  Un niño y su padre juegan con bloques de madera construyendo una pirámide plana, donde cada capa inferior contiene un bloque más que la capa superior.
-
+ 
  Escribe un programa que lea la cantidad de bloques que tienen, y genere la altura de la pirámide que se puede construir con estos bloques.
-
- Nota: La altura se mide por el número de capas completas
+ NOTA: La altura se mide por el número de capas completas
 -----------------------------
 '''
 bloques = int("Ingrese el número de bloques:"))
